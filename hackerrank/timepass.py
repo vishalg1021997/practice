ee3d9c882f8c8a6dc378b771c636e82d21bdf36b
Vishal Gupta <83306636+vishalg1021997@users.noreply.github.com>
2021-05-08 13:50:33 +0530

    Add files via upload

---
+++ hackerrank/timepass.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+l = []
+for i in range(0,x+1):
+    for j in range(0,y+1):
+        for k in range(0,z+1):
+            if i + j + k != n:
+                l.append([i,j,k])
+print(l)
+
